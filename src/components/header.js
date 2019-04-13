c78534363cd9008dacdc6125970f53d444ec85f2
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2019-04-13 23:25:16 +0200

    Modify gatsby starter code.Use styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,35 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
+const Container = styled.header`
+  background: rebeccapurple;
+  marginBottom: 1.45rem;
+`
+const SiteTitleWrapper = styled.div`
+  margin: 0 auto;
+  maxWidth: 960;
+  padding: 1.45rem 1.0875rem;
+`
+const SiteTitle = styled.h1`
+  margin: 0;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <Container>
+    <SiteTitleWrapper>
+      <SiteTitle>
+        <StyledLink to="/">
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+        </StyledLink>
+      </SiteTitle>
+    </SiteTitleWrapper>
+  </Container>
 )
 
 Header.propTypes = {
